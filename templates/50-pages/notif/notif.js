f4744a1cc67847b7d61695b1ac7dc1bd2c9477f0
dagrut <dagrut@dagrut.info>
2016-10-26 16:50:11 +0400

    Fixed popup fadein/out animations.

--- templates/50-pages/notif/notif.js
+++ templates/50-pages/notif/notif.js
@@ -2,19 +2,22 @@ tplEvents['notif'] = function($notif, vars) {
 	var duration = vars.duration || 5000;
 	
 	var curTO = setTimeout(showNotif, 100);
+	var animDur = 300;
 	
 	function showNotif() {
+		var notifh = $notif.height();
+		var margtop = $notif.css('margin-top');
+		$notif.height(0).css("opacity", 0);
 		$notif.addClass('notif-visible');
+		$notif.animate({ height: notifh, opacity: 1 }, animDur);
 		curTO = setTimeout(hideNotif, duration);
 	}
 	
 	function hideNotif() {
-		$notif.removeClass('notif-visible');
-		curTO = setTimeout(deleteNotif, 1000);
-	}
-	
-	function deleteNotif() {
-		$notif.remove();
+		$notif.animate({ height: 0, opacity: 0 }, animDur, function() {
+			$notif.removeClass('notif-visible');
+			$notif.remove();
+		});
 	}
 	
 	$notif.click(function(event) {
